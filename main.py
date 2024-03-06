8e26c43ee2186011e22af45e6c9515ed2bbe42f7
Maksym Bezhevets <begik4@gmail.com>
2024-03-06 18:51:18 +0200

    Solution

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from fastapi import FastAPI
+import telebot
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from telebot.apihelper import ApiTelegramException
 
 app = FastAPI()
 
 
+class TelegramMessage(BaseModel):
+    bottoken: str
+    chatid: str
+    message: str
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/send/")
+def send_message(data: TelegramMessage):
+    bot = telebot.TeleBot(data.bottoken)
+    try:
+        bot.send_message(data.chatid, data.message, parse_mode="Markdown")
+        return {"status": "Message sent successfully"}
+    except ApiTelegramException as error:
+        raise HTTPException(status_code=500, detail=f"Telegram API error: {error}")
